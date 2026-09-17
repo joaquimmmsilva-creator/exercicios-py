c61c569dca21095408b887c911c633473126115b
joaquimmmsilva-creator <joaquim_mm_silva@estudante.sesisenai.org.br>
2026-09-17 09:42:10 -0300

    quinto exercicio, valor do produto baseado na quantidade e valor

---
+++ parte1-variaveis/ex05.py
@@ -0,0 +1,6 @@
+precoproduto = float(input("digite o preço do produto: "))
+quantidade = int(input("digite a quantidade comprada: "))
+
+total = precoproduto * quantidade
+
+print("o valor total da compra é:",f"{total:,.2f}")

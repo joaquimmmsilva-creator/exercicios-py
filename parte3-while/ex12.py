3d71b2d56644e72189aedf145822723f4a73fe30
joaquimmmsilva-creator <joaquim_mm_silva@estudante.sesisenai.org.br>
2026-09-17 11:27:45 -0300

    exercicio 12, usuario digita o numero até digitar 0 e no final o programa faz a soma

---
+++ parte3-while/ex12.py
@@ -0,0 +1,12 @@
+numero = int(input("Digite um número: "))
+
+
+soma = 0
+while numero != 0:
+    numero = int(input("Digite outro número: "))
+    soma = soma + numero
+
+
+
+
+print("A soma dos números digitados é:", soma)

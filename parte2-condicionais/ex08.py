17ff9f444a2997b88540ddfd44301b16e80a0744
joaquimmmsilva-creator <joaquim_mm_silva@estudante.sesisenai.org.br>
2026-09-17 09:53:11 -0300

    exercicio 08, verificação de numero, positivo, negativo ou igaul a zero

---
+++ parte2-condicionais/ex08.py
@@ -0,0 +1,9 @@
+numero = int(input("digite um número: "))
+
+if numero <= 0:
+    print("o número é negativo")
+else: 
+    print("o número é positivo")
+
+if numero == 0:
+    print("o número é zero")

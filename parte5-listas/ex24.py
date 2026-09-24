664b8c92ddac581cbb8514b71734f02e23797ab5
joaquimmmsilva-crator <joaquim_mm_silva@estudante.sesisenai.org.br>
2026-09-24 00:32:40 -0300

    exercicio 24, quantos numeros sao maiores que 10

---
+++ parte5-listas/ex24.py
@@ -0,0 +1,9 @@
+numeros = [5, 12, 8, 20, 3, 15]
+
+quantidade = 0
+
+for numero in numeros:
+    if numero > 10:
+        quantidade += 1
+
+print("a quantidade de itens maiores que 10 é:", quantidade)
